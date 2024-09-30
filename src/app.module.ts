3cf3cc783f8eda87f2547e98a3e2ddc5e7e394a0
Haster1337 <hasterpro@gmail.com>
2024-10-01 00:39:28 +0400

    dotenv and typeorm+pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ConfigModule} from "@nestjs/config";
+import * as process from "node:process";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath:  process.env.NODE_ENV === 'production' ? '.env.production' : '.env.local',
+            isGlobal: true
+        })
+        , TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            password: process.env.DB_PASSWORD,
+            username: process.env.DB_USERNAME,
+            entities: [],
+            synchronize: true,
+            database: process.env.DB_NAME,
+            ssl: !!+process.env.DB_SSL
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
+
+
+
+
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.SERVER_PORT || 7000;
+  await app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
+
 bootstrap();
