b91eacba6d52497c6c6d72308f1ba7d4c1c3bc84
Haster1337 <hasterpro@gmail.com>
2024-10-07 01:01:54 +0400

    test corst

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const PORT = process.env.SERVER_PORT || 7000;
     app.use(cookieParser())
-    app.enableCors({origin: "*", credentials: true});
+    app.enableCors({origin: "http://localhost:5173/", credentials: true});
     await app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 
